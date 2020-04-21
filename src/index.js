adf9dd97914404e38835369eaeb7230d7b58c77e
Cl3r1k <Cl3r1k2@gmail.com>
2020-04-21 14:22:49 +0700

    feat: implement basic project structure (webpack + packages)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './scss/main.scss';
+
+const initApp = () => {
+  // console.log('%c Completely loaded...', 'color: red;');
+};
+
+// eslint-disable-next-line no-undef
+document.addEventListener('DOMContentLoaded', () => {
+  initApp();
+});
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          failOnError: true,
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        exclude: /\.module\.(css|scss|sass)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.module\.(css|scss|sass)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              sourceMap: true,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      // filename: './index.html',
+    }),
+    new CopyWebpackPlugin([
+      { from: 'assets', to: 'assets' },
+      // { from: 'other', to: 'public' },
+    ]),
+  ],
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+  },
+};
+
+module.exports = config;
