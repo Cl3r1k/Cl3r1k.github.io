f3c32c81766792307246f7bc71a93caab5e9276b
Cl3r1k <Cl3r1k2@gmail.com>
2020-04-22 12:29:54 +0700

    feat: implement nav-bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
+/* eslint-disable no-undef */
+
+import throttle from 'lodash/throttle';
+
 import './scss/main.scss';
 
-const initApp = () => {
-  // console.log('%c Completely loaded...', 'color: red;');
+const navBarElement = document.getElementById('navBarId');
+const mainNavLinks = document.querySelectorAll('nav ul li a');
+// const mainDivs = document.querySelectorAll('main div');
+
+const scrollHandler = () => {
+  // console.log('evt: ', evt);
+  // console.log('document.scrollTop: ', document.scrollTop());
+  // console.log('window.scrollY: ', window.scrollY);
+
+  const scrollPosition = window.scrollY;
+  navBarElement.classList.toggle('scrolled', scrollPosition > 100);
+
+  mainNavLinks.forEach(link => {
+    const divElement = document.querySelector(link.hash);
+
+    const isScrollInDivArea =
+      divElement.offsetTop <= scrollPosition &&
+      divElement.offsetTop + divElement.offsetHeight > scrollPosition;
+
+    link.parentElement.classList.toggle('active', isScrollInDivArea);
+  });
+
+  // console.log('mainDivs: ', mainDivs);
 };
 
-// eslint-disable-next-line no-undef
+// const logIt = () => {
+//   console.log('logIt() called');
+//   scrollHandler();
+// };
+
+const throttledScrollHandler = throttle(scrollHandler, 100);
+
+const initApp = () => {
+  document.addEventListener('scroll', throttledScrollHandler);
+
+  console.log('navBarElement: ', navBarElement);
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   initApp();
 });
